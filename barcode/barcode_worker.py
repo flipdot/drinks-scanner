1419031a3c7bd68b9d7d7bea9dd2676415d9dbca
Daniel Huhn <daniel@danielhuhn.de>
2016-04-26 23:48:45 +0200

    added counter per day

--- barcode/barcode_worker.py
+++ barcode/barcode_worker.py
@@ -1,3 +1,5 @@
+import datetime
+
 from database.storage import get_session
 from database.models.scan_event import ScanEvent
 
@@ -13,8 +15,27 @@ class Worker:
         session.add(ev)
         session.commit()
 
+    def __get_drinks_count(self, date):
+        session = get_session()
+        result = session.execute("""
+          SELECT
+            timestamp::date date,
+            COUNT(timestamp) amountd
+          FROM scanevent
+          WHERE timestamp::date = '{today}'
+          GROUP BY timestamp::date
+        """.format(today=date)).fetchall()
+
+        row = result[0]
+        date = row[0]
+        amount = row[1]
+
+        return amount
+
     def __print_flipdots(self, barcode):
-        print '|-| '+barcode+' |-|'
+        today = datetime.date.strftime(datetime.datetime.now(), '%Y-%m-%d')
+        amount = self.__get_drinks_count(today)
+        print '|-| '+barcode + ' - '+ str(amount) +' |-|'
 
     def on_barcode(self, barcode):
         self.__write_db(barcode)
